f2c2ba54db4724cbdfc917920bf28890b4183468
Mitchell Lopez <mitchellslopez@gmail.com>
2019-08-07 10:28:13 -0500

    Albums component loads as intended

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -11,7 +11,6 @@ class Album extends React.Component {
   render() {
     return (
       <div className="album-parent">
-        {/* will only render when width is less than 872px */}
         <div className="mobile">
           <div className="mobile-label">Landscape</div>
           <img
@@ -32,7 +31,7 @@ class Album extends React.Component {
           <div className="mobile-label">Moon</div>
           <img
             className="mobile-img"
-            src="https://photos.smugmug.com/BEST/i-6D7jwww/0/dd501db8/X2/IMG_3142-X2.jpg"
+            src="https://photos.smugmug.com/BEST/i-VgnPjVk/0/c420512a/XL/IMG_2844-XL.jpg"
             alt="moon"
           />
         </div>
@@ -59,18 +58,6 @@ class Album extends React.Component {
             src="https://photos.smugmug.com/BEST/i-Nbnc8dK/0/bd6ae82c/XL/IMG_6931-XL.jpg"
             alt="Other"
           />
-        </div>
-        {/* Will only render on window width greater than 872 */}
-        <div className="album-row-1">
-          <div className="album landscape">Landscape</div>
-          <div className="album sunset">Sunset</div>
-          <div className="album wildlife">Wildlife</div>
-        </div>
-        <div className="album-row-2">
-          <div className="album moon">Moon</div>
-          <div className="album travel">Travel</div>
-          <div className="album other">Other</div>
-
         </div>
       </div>
     );
